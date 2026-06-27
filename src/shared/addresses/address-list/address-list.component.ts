c12ec0d1af6ad4523466059744423edf74244d41
@Amin&1378 <aminviper1378@gmail.com>
2026-06-27 19:44:04 +0330

    feat(profile): add addres user component

--- src/shared/addresses/address-list/address-list.component.ts
+++ src/shared/addresses/address-list/address-list.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+interface Address {
+  id: number;
+  province: string;
+  city: string;
+  street: string;
+  postalCode: string;
+  plaque: string;
+  isDefault: boolean;
+  created_at: string;
+}
 
 @Component({
   selector: 'app-address-list',
   templateUrl: './address-list.component.html',
-  styleUrl: './address-list.component.css'
+  styleUrls: ['./address-list.component.css'],
 })
-export class AddressListComponent {
+export class AddressListComponent implements OnInit {
+  addresses: Address[] = [];
+  loading = true;
+  showForm = false;
+  editingAddress: Address | null = null;
 
+  form = {
+    province: '',
+    city: '',
+    street: '',
+    postalCode: '',
+    plaque: '',
+    isDefault: false,
+  };
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() { this.loadAddresses(); }
+
+  get headers() {
+    const token = sessionStorage.getItem('accessToken');
+    return new HttpHeaders({ Authorization: `Bearer ${token}` });
+  }
+
+  loadAddresses() {
+    this.loading = true;
+    this.http.get<Address[]>('http://localhost:4000/address', { headers: this.headers }).subscribe({
+      next: (data) => { this.addresses = data; this.loading = false; },
+      error: () => { this.loading = false; },
+    });
+  }
+
+  openAdd() {
+    this.editingAddress = null;
+    this.form = { province: '', city: '', street: '', postalCode: '', plaque: '', isDefault: false };
+    this.showForm = true;
+  }
+
+  openEdit(address: Address) {
+    this.editingAddress = address;
+    this.form = {
+      province: address.province,
+      city: address.city,
+      street: address.street,
+      postalCode: address.postalCode,
+      plaque: address.plaque || '',
+      isDefault: address.isDefault,
+    };
+    this.showForm = true;
+  }
+
+  saveAddress() {
+    const body = new URLSearchParams();
+    Object.entries(this.form).forEach(([k, v]) => body.set(k, String(v)));
+    const headers = this.headers.set('Content-Type', 'application/x-www-form-urlencoded');
+
+    if (this.editingAddress) {
+      this.http.patch(`http://localhost:4000/address/${this.editingAddress.id}`, body.toString(), { headers }).subscribe({
+        next: () => { this.showForm = false; this.loadAddresses(); },
+      });
+    } else {
+      this.http.post('http://localhost:4000/address', body.toString(), { headers }).subscribe({
+        next: () => { this.showForm = false; this.loadAddresses(); },
+      });
+    }
+  }
+
+  deleteAddress(id: number) {
+    if (!confirm('آیا از حذف این آدرس مطمئن هستید؟')) return;
+    this.http.delete(`http://localhost:4000/address/${id}`, { headers: this.headers }).subscribe({
+      next: () => this.loadAddresses(),
+    });
+  }
+
+  cancel() { this.showForm = false; }
 }
